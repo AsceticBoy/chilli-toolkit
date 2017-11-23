541a0ce9f5bcc24939db5bba978be5cbf227f720
Ascetic <398121373@qq.com>
2017-11-23 16:45:27 +0800

    feat(es6-additional): add finally / done func to es6-additional

--- src/es6-additional.js
+++ src/es6-additional.js
@@ -1,14 +1,31 @@
-'use strict'
+'use strict';
 
-let IPromise = require('./core.js')
-let Shared = require('./shared.js')
+let IPromise = require('./core.js');
+let Shared = require('./shared.js');
 
-module.exports = IPromise
+module.exports = IPromise;
 
-IPromise.prototype.done = function () {
-  // TODO
-}
+// Prevent last IPromise error can not catch
+IPromise.prototype.done = function(onFulfilled, onRejected) {
+  let self = arguments.length ? this.then.apply(this, arguments) : this;
+  self.then(null, function(err) {
+    setTimeout(() => {
+      throw err;
+    }, 0);
+  });
+};
 
-IPromise.prototype.finally = function () {
-  // TODO
-}
+// whatever fulfilled or rejected exec callback
+IPromise.prototype.finally = function(callback) {
+  // return a IPromise
+  this.then(
+    function(val) {
+      IPromise.resolve(callback()).then(() => val);
+    },
+    function(err) {
+      IPromise.resolve(callback()).then(() => {
+        throw err;
+      });
+    }
+  );
+};

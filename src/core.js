d524a2443e85e0b98f6ad4f2343930b792b00f04
Ascetic <398121373@qq.com>
2017-11-23 15:39:55 +0800

    feat(all/race): add all/race function in es6-extensions

--- src/core.js
+++ src/core.js
@@ -1,75 +1,79 @@
-'use strict'
+'use strict';
 
-let Shared = require('./shared.js')
+let Shared = require('./shared.js');
 
-module.exports = IPromise
+module.exports = IPromise;
 
 /**
  * ----- IPromise Codebase -----
- * 
+ *
  * Promise Code Implement Standard https://promisesaplus.com/#point-46
- * 
+ *
  * @param { Function } executor => I want to do in this function
  */
 function IPromise(executor) {
-  const self = this
+  const self = this;
   if (!Shared.isObject(self)) {
-    throw new TypeError('IPromise must be a object')
+    throw new TypeError('IPromise must be a object');
   }
   if (!Shared.isFunction(executor)) {
-    throw new TypeError('executor must be a function')
+    throw new TypeError('executor must be a function');
   }
-  this.currentState = Shared.PENDING // default
-  this.valOonrErr = null // if fulfilled result to it else rejected error to it
-  this.onFulfilledCallback = [] // if current state PENDING push the func wait onFulfilled to exec
-  this.onRejectedCallback = [] // if current state PENDING push the func wait onRejected to exec
-  
+  this.currentState = Shared.PENDING; // default
+  this.valOonrErr = null; // if fulfilled result to it else rejected error to it
+  this.onFulfilledCallback = []; // if current state PENDING push the func wait onFulfilled to exec
+  this.onRejectedCallback = []; // if current state PENDING push the func wait onRejected to exec
+
   // safely executor when executor not a empty function
   if (Shared.noop !== executor) {
-    safelyToExecutor(self, executor) 
+    safelyToExecutor(self, executor);
   }
 }
 
 /**
  * onResolved or onRejected only one called once
- * 
+ *
  * @param { object } self current IPromise
  * @param { function } executor this's user operater
  */
 function safelyToExecutor(self, executor) {
-  let done = false
+  let done = false;
   try {
     executor(
       function(result) {
-        if (done) return
-        done = true
-        doResolve.call(self, result)
+        if (done) return;
+        done = true;
+        doResolve.call(self, result);
       }, // doResolve
       function(error) {
-        if (done) return
-        done = true
-        doReject.call(self, error)
+        if (done) return;
+        done = true;
+        doReject.call(self, error);
       } // doReject
-    )
+    );
   } catch (error) {
-    if (done) return
-    done = true
-    doReject.call(self, error)
+    if (done) return;
+    done = true;
+    doReject.call(self, error);
   }
 }
 
 /**
  * if resolve(result) result is a thenable return or wait util onFulfilled or onRejected
- * 
- * @param { all } maybeThenable 
+ *
+ * @param { all } maybeThenable
  */
 function safelyToThen(maybeThenable) {
   // Promise Standard 2.3.3.1
-  let then = maybeThenable && maybeThenable.then
-  if (maybeThenable && (Shared.isFunction(maybeThenable) || Shared.isObject(maybeThenable)) && Shared.isFunction(then)) {
+  let then = maybeThenable && maybeThenable.then;
+  if (
+    maybeThenable &&
+    (Shared.isFunction(maybeThenable) || Shared.isObject(maybeThenable)) &&
+    Shared.isFunction(then)
+  ) {
     return function applyThen() {
-      then.apply(maybeThenable, arguments)
-    }
+      then.apply(maybeThenable, arguments);
+    };
   }
 }
 
@@ -82,59 +86,66 @@ function doResolve(result) {
   try {
     // Promise Standard 2.3.2 and 2.3.3 can be merge
     // if result is a thenable(.then)
-    let then = safelyToThen(result)
+    let then = safelyToThen(result);
     if (then) {
       // Promise Standard 2.3.3.3
-      safelyToExecutor(this, then)
+      safelyToExecutor(this, then);
     } else {
       // commonly case
-      this.currentState = Shared.FULFILLED
-      this.valOrErr = result
+      this.currentState = Shared.FULFILLED;
+      this.valOrErr = result;
       for (let i = 0; i < this.onFulfilledCallback.length; i++) {
-        this.onFulfilledCallback[i](result)
+        this.onFulfilledCallback[i](result);
       }
     }
+    // case: when then(doResolve, doReject) -> asyncToResolveOrReject -> func(arg) -> return self
+    return this;
   } catch (error) {
-    doReject.call(this, error)
+    return doReject.call(this, error);
   }
 }
 
 function doReject(error) {
-  this.currentState = Shared.REJECTED
-  this.valOrErr = error
+  this.currentState = Shared.REJECTED;
+  this.valOrErr = error;
   for (let i = 0; i < this.onRejectedCallback.length; i++) {
-    this.onRejectedCallback[i](error)
+    this.onRejectedCallback[i](error);
   }
+  return this;
 }
 
 function asyncToResolveOrReject(self, func, arg) {
   // Promise is a Microtask, but setTimeout is a Macrotask, strictly use process.nextTick will be better, but browser not support
-  setTimeout(function () {
+  setTimeout(function() {
     try {
-      let result = func(arg)
-      if (result === self) { // Promise Standard 2.3.1
-        return doReject.call(self, new TypeError('Can not resolve \'Promise\' itself'))
+      let result = func(arg);
+      if (result === self) {
+        // Promise Standard 2.3.1
+        return doReject.call(
+          self,
+          new TypeError("Can not resolve 'Promise' itself")
+        );
       }
-      doResolve.call(self, result)
+      doResolve.call(self, result);
     } catch (error) {
-      doReject.call(self, error)
+      doReject.call(self, error);
     }
-  }, 0)
+  }, 0);
 }
 
 /**
  * IPromise prototype func then renturn a new IPromise Instance
- * 
- * @param { function } onFulfilled 
- * @param { function } onRejected 
+ *
+ * @param { function } onFulfilled
+ * @param { function } onRejected
  */
-IPromise.prototype.then = function (onFulfilled, onRejected) {
-  let self = this
+IPromise.prototype.then = function(onFulfilled, onRejected) {
+  let self = this;
   /**
    * self.constructor otherwise IPromise --> different Promise implement need communicate to each other
    * noop --> only new a 'Promise' then inject currentState/valOrErr etc...
    */
-  let ownerPromise = new self.constructor(Shared.noop)
+  let ownerPromise = new self.constructor(Shared.noop);
 
   /**
    * Value Penetration
@@ -145,22 +156,31 @@ IPromise.prototype.then = function (onFulfilled, onRejected) {
    *     .then((data) => {console.log(data)})
    * Value Penetration means log '8' otherwise 'undefined'
    */
-  onFulfilled = Shared.isFunction(onFulfilled) ? onFulfilled : function(val) { return val }
-  onRejected = Shared.isFunction(onRejected) ? onRejected : function(err) { throw err }
+  onFulfilled = Shared.isFunction(onFulfilled)
+    ? onFulfilled
+    : function(val) {
+        return val;
+      };
+  onRejected = Shared.isFunction(onRejected)
+    ? onRejected
+    : function(err) {
+        throw err;
+      };
 
   if (self.currentState !== Shared.PENDING) {
-    let func = self.currentState === Shared.FULFILLED ? onFulfilled : onRejected
+    let func =
+      self.currentState === Shared.FULFILLED ? onFulfilled : onRejected;
     // valOrErr stable self's status or func's thenable determine it's status
-    asyncToResolveOrReject(ownerPromise, func, self.valOonrErr)
+    asyncToResolveOrReject(ownerPromise, func, self.valOonrErr);
   } else {
     // push pending func
-    self.onFulfilledCallback.push(function (value) {
-      asyncToResolveOrReject(ownerPromise, onFulfilled, value)
-    })
-    self.onRejectedCallback.push(function (error) {
-      asyncToResolveOrReject(ownerPromise, onRejected, error)
-    })
+    self.onFulfilledCallback.push(function(value) {
+      asyncToResolveOrReject(ownerPromise, onFulfilled, value);
+    });
+    self.onRejectedCallback.push(function(error) {
+      asyncToResolveOrReject(ownerPromise, onRejected, error);
+    });
   }
 
-  return ownerPromise
-}
+  return ownerPromise;
+};
--- src/es6-extensions.js
+++ src/es6-extensions.js
@@ -1,63 +1,107 @@
-'use strict'
+'use strict';
 
-let IPromise = require('./core.js')
-let Shared = require('./shared.js')
+let IPromise = require('./core.js');
+let Shared = require('./shared.js');
 
-module.exports = IPromise
+module.exports = IPromise;
 
 function valueToIPromise(value, state) {
-  let instance = new IPromise(Shared.noop)
-  instance.currentState = state
-  instance.valOonrErr = value
-  return instance
+  let instance = new IPromise(Shared.noop);
+  instance.currentState = state;
+  instance.valOonrErr = value;
+  return instance;
 }
 
-// IPromise Instance catch 
-IPromise.prototype.catch = function (onRejected) {
-  return this.then(null, onRejected)
-}
+// IPromise Instance catch
+IPromise.prototype.catch = function(onRejected) {
+  return this.then(null, onRejected);
+};
 
 // IPromise.resolve return a fulfilled IPromise
-IPromise.resolve = function (value) {
-  let self = this
+IPromise.resolve = function(value) {
+  let self = this;
   // if value is a IPromise return self
-  if (value instanceof IPromise) return value
+  if (value instanceof IPromise) return value;
 
   // some special value
-  if (value === undefined) return valueToIPromise(value, Shared.FULFILLED) // no value
-  if (value === null) return valueToIPromise(value, Shared.FULFILLED)
+  if (value === undefined) return valueToIPromise(value, Shared.FULFILLED); // no value
+  if (value === null) return valueToIPromise(value, Shared.FULFILLED);
 
   // value is thenable object
   if (Shared.isFunction(value) || Shared.isObject(value)) {
-    let then = value.then
+    let then = value.then;
     try {
       if (Shared.isFunction(then)) {
-        return new IPromise(then.bind(value))
+        return new IPromise(then.bind(value));
       }
     } catch (error) {
-      return new IPromise(function (resolve, reject) {
-        reject(error)
-      })
+      return new IPromise(function(resolve, reject) {
+        reject(error);
+      });
     }
   }
 
-  return valueToIPromise(value, Shared.FULFILLED) // no Object no Function maybe a 'string' etc...
-}
+  return valueToIPromise(value, Shared.FULFILLED); // no Object no Function maybe a 'string' etc...
+};
 
 // IPromise.resolve return a rejected IPromise
-IPromise.reject = function (error) {
+IPromise.reject = function(error) {
   // if error is a thenable need not invoke then() return error self
-  return valueToIPromise(error, Shared.REJECTED)
-}
+  return valueToIPromise(error, Shared.REJECTED);
+};
 
 // IPromise.all wait allPromise stable then return new IPromise
-IPromise.all = function (Iteratable) {
-  // trigger a Array
-  let array = Array.prototype.slice.call(Iteratable)
+IPromise.all = function(Iteratable) {
+  try {
+    // trigger a Array
+    let array = Array.prototype.slice.call(Iteratable);
+    return new IPromise(function(resolve, reject) {
+      if (!array.length) return resolve([]);
+      let rejectDone = false;
+      let count = 0;
+      function allResolver(idx, val) {
+        if (val && (Shared.isObject(val) || Shared.isFunction(val))) {
+          // only support IPromise
+          if (val instanceof IPromise && val.then === IPromise.prototype.then) {
+            if (val.currentState === Shared.FULFILLED)
+              return allResolver(idx, val.valOonrErr);
+            if (val.currentState === Shared.REJECTED)
+              return reject(val.valOonrErr);
+            // pending
+            val.then(function(val) {
+              allResolver(idx, val);
+            }, reject);
+          }
+        }
+        array[idx] = val;
+        if (++count === array.length) {
+          resolve(array);
+        }
+      }
+      for (let i = 0; i < array.length; i++) {
+        allResolver(i, array[i]);
+      }
+    });
+  } catch (error) {
+    return new IPromise(function(resolve, reject) {
+      reject(error);
+    });
+  }
+};
 
-  // TODO
-}
-
-IPromise.race = function (Iteratable) {
-  // TODO
-}
+// IPromise.race means one Instance changeState then exec callback
+IPromise.race = function(Iteratable) {
+  try {
+    let array = Array.prototype.slice.call(Iteratable);
+    return new IPromise(function(resolve, reject) {
+      if (!array.length) return resolve(Iteratable); // go back self like '' true false etc...
+      for (let i = 0; i < array.length; i++) {
+        IPromise.resolve(array[i]).then(resolve, reject);
+      }
+    });
+  } catch (error) {
+    return new IPromise(function(resolve, reject) {
+      reject(error);
+    });
+  }
+};
